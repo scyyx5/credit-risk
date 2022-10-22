b236b56afb52da239cee7100fd2f7726434b0f57
Yujiao <yxx132@student.bham.ac.uk>
2022-10-22 16:46:22 +0800

    display HTML file

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,6 +5,11 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.io.File;
+import java.net.MalformedURLException;
 
 
 public class HelloController {
@@ -13,12 +18,17 @@ public class HelloController {
     private Pane dr_agePane;
 
     @FXML
-    private Pane dr_calPane;
-
-
+    private WebView dr_ageWebView;
+    WebEngine dr_agewebEngine = null;
 
     @FXML
     void dr_ageTab(Event event) {
+
+    }
+
+
+    @FXML
+    void ondr_agePredictedButtonClick(ActionEvent event) {
 
     }
 
@@ -28,17 +38,31 @@ public class HelloController {
     }
 
     @FXML
-    void ondr_agePredictedButtonClick(ActionEvent event) {
+    private Pane dr_calPane;
 
-    }
+    @FXML
+    private WebView dr_calWebView;
+    WebEngine dr_calwebEngine = null;
 
     @FXML
     void ondr_calPredictedButtonClick(ActionEvent event) {
 
+
     }
 
 
+    public void initialize() throws MalformedURLException {
 
+        dr_agewebEngine = dr_ageWebView.getEngine();
+        File dr_age_file = new File("res\\dr_age.html");
+        dr_agewebEngine.load(dr_age_file.toURI().toURL().toString());
+        System.out.println("miao");
+
+        dr_calwebEngine = dr_calWebView.getEngine();
+        File dr_cal_file = new File("res\\dr_cal.html");
+        dr_calwebEngine.load(dr_cal_file.toURI().toURL().toString());
+        System.out.println("miaomiao");
+    }
 }
 
 
